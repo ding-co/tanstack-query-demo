8f0adf59cbb9176537ac72362ce6e3a72139171c
ding-co <pangoons@naver.com>
2024-06-13 19:59:18 +0900

    update: queryClient default options to conservative

--- src/react-query/query-client.ts
+++ src/react-query/query-client.ts
@@ -1,15 +1,8 @@
 import { QueryClient } from '@tanstack/react-query';
 
 const makeQueryClient = () => {
-  return new QueryClient({
-    defaultOptions: {
-      queries: {
-        retry: false,
-        refetchOnWindowFocus: false,
-        // staleTime: 60 * 1000,
-      },
-    },
-  });
+  // conservative options
+  return new QueryClient();
 };
 
 let browserQueryClient: QueryClient | undefined = undefined;
