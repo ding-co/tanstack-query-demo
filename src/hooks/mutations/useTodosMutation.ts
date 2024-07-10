2fe45a727936bae478e7c79f89ee1cf22d0033d5
ding-co <pangoons@naver.com>
2024-07-10 19:21:56 +0900

    update: delete unstabled type with rest parameters for options

--- src/hooks/mutations/useTodosMutation.ts
+++ src/hooks/mutations/useTodosMutation.ts
@@ -1,16 +1,10 @@
 import { deleteTodo, patchTodo, postTodo } from '@/api';
 import { Todo } from '@prisma/client';
-import {
-  UseMutationOptions,
-  useMutation,
-  useQueryClient,
-} from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { queryKeys } from '@/react-query/query-keys';
 
-export const usePostTodoMutation = (
-  mutationOptions?: UseMutationOptions<void, Error, string>,
-) => {
+export const usePostTodoMutation = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
@@ -20,13 +14,10 @@ export const usePostTodoMutation = (
         queryKey: [queryKeys.todos],
       });
     },
-    ...mutationOptions,
   });
 };
 
-export const usePatchTodoMutation = (
-  mutationOptions?: UseMutationOptions<void, Error, number>,
-) => {
+export const usePatchTodoMutation = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
@@ -57,13 +48,10 @@ export const usePatchTodoMutation = (
         queryKey: [queryKeys.todos],
       });
     },
-    ...mutationOptions,
   });
 };
 
-export const useDeleteTodoMutation = (
-  mutationOptions?: UseMutationOptions<void, Error, number>,
-) => {
+export const useDeleteTodoMutation = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
@@ -73,6 +61,5 @@ export const useDeleteTodoMutation = (
         queryKey: [queryKeys.todos],
       });
     },
-    ...mutationOptions,
   });
 };
